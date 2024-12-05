8aae906cff2d76511ed968550a02ac09fa0dcb0e
Leo <a12232856@unet.univie.ac.at>
2024-12-05 12:37:42 +0100

    Add Passenger, Ticket and Flight types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,4 +1,28 @@
 export enum UserType {
     Passenger = 'Passenger',
     FlightDispatcher = 'Flight Dispatcher'
-}
+};
+
+export type Passenger = {
+    passportNumber: string;
+    firstName: string;
+    lastName: string;
+    nationality: string;
+};
+
+export type Ticket = {
+    ticketId: number;
+    seatNumber: string;
+    ticketClass: string;
+    passportNumber: string;
+    flightNumber: string;
+};
+
+export type Flight = {
+    flightNumber: string;
+    departureAirport: string;
+    destinationAirport: string;
+    arrivalTime: Date;
+    destinationTime: Date;
+    numberOfSeats: number;
+};
