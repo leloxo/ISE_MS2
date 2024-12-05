4519ec86f3818b00fa73e891c557a556b543a9ee
Leo <a12232856@unet.univie.ac.at>
2024-12-05 12:39:20 +0100

    Implement mysql connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,6 +2,14 @@
 
 import mysql from 'mysql2/promise';
 
-export const mysqlConnection = mysql.createPool({
-    // TODO
+const mysqlConnection = mysql.createPool({
+    host: process.env.MYSQL_DB_HOST,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
+
+export default mysqlConnection;
