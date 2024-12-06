5049f3d49c167bd409407374b2500f80e6717538
Leo <a12232856@unet.univie.ac.at>
2024-12-06 14:07:44 +0100

    Add DatabasePopulationButton to Header component, Update styling

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './header.module.scss';
 import { UserType } from '../../types/types';
+import DatabasePopulationButton from '../DatabasePopulationButton/DatabasePopulationButton';
 
 interface HeaderProps {
     onSelectUser: (user: UserType) => void;
@@ -14,7 +15,10 @@ const Header: React.FC<HeaderProps> = ({ onSelectUser, currentUser }) => {
 
     return (
         <div className={styles.headerContainer}>
-            <h1>{currentUser === UserType.Passenger ? 'Passenger Dashboard' : 'Flight Dispatcher Dashboard'}</h1>
+            <h1>Logged in as {currentUser === UserType.Passenger ? 'Passenger' : 'Flight Dispatcher'}</h1>
+
+            <DatabasePopulationButton />
+            
             <select value={currentUser} onChange={handleChange}>
                 <option value={UserType.Passenger}>Passenger</option>
                 <option value={UserType.FlightDispatcher}>Flight Dispatcher</option>
