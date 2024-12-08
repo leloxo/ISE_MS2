e7eaa4b37443e95fc917b1e3278c81c8dcc319c3
Leo <a12232856@unet.univie.ac.at>
2024-12-08 16:34:03 +0100

    Add TicketBookingForm component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import styles from './app.module.scss';
 import Header from './components/Header/Header';
 import { UserType } from './types/types';
 import Title from './components/Title/Title';
+import TicketBookingForm from './components/TicketBookingForm/TicketBookingForm';
 
 const App: React.FC = () => {
     const [currentUser, setCurrentUser] = useState<UserType>(UserType.Passenger);
@@ -19,7 +20,7 @@ const App: React.FC = () => {
                 <div className={styles.mainContainer}>
                     {currentUser === UserType.Passenger ? (
                         <div> 
-
+                            <TicketBookingForm />
                         </div>
                     ) : (
                         <div> 
