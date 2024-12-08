3909f188813bef4c16c9ca91f2f10fe5bb99e2ed
Leo <a12232856@unet.univie.ac.at>
2024-12-08 16:34:17 +0100

    Update flightApi

--- frontend/src/api/flightApi.ts
+++ frontend/src/api/flightApi.ts
@@ -1,13 +1,29 @@
 import { Flight } from '../types/types';
 import axiosInstance from './axiosInstance';
 
-export const fetchFlights = async (): Promise<Flight> => {
+export const fetchFlights = async (): Promise<Flight[]> => {
     try {
-        const response = await axiosInstance.get('/flight');
+        const response = await axiosInstance.get('/flights');
         console.log(response.data);
         return response.data;
     } catch (error) {
-        console.error('Error populating database: ', error);
+        console.error('Error fetching flights: ', error);
+        throw error;
+    }
+};
+
+export const fetchFlightsByAirport = async (departureAirport: string, destinationAirport: string): Promise<Flight[]> => {
+    try {
+        const response = await axiosInstance.get('/flights/airport', {
+            params: {
+                departureAirport,
+                destinationAirport,
+            },
+        });
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching flights: ', error);
         throw error;
     }
 };
